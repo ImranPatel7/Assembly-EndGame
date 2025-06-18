83e5995fa2c724649a123834520439c2ce61629a
Imran Patel <imranp4tel@gmail.com>
2025-06-18 17:01:27 +0530

    static game-status and language-chips bit done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { languages } from "./languages";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const languageElements = languages.map((lang) => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
+    return (
+      <span className="chip" style={styles} key={lang.name}>
+        {lang.name}
+      </span>
+    );
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <main>
+      <header>
+        <h1>Assembly:EndGame</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      </header>
+      <section className="game-status">
+        <h2>You Win!</h2>
+        <p>..WellDone..</p>
+      </section>
+      <section className="language-chips">{languageElements} </section>
+    </main>
+  );
+};
 
-export default App
+export default App;
